47acb873c035a6f490199e6c107430a2b2f9b640
Cobus Carstens <cobus.carstens@gmail.com>
2013-06-30 23:11:42 +0200

    Start script modified to use the sys.config file

--- estatsdb/start.sh
+++ estatsdb/start.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 cd `dirname $0`
-exec erl -pa $PWD/ebin $PWD/deps/*/ebin -boot start_sasl -s reloader -s estatsdb
+exec erl -pa $PWD/ebin $PWD/deps/*/ebin -config rel/files/sys.config -boot start_sasl -s reloader -s estatsdb -name mira_sbm@127.0.0.1
